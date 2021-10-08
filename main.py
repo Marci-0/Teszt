d3a8ece81c7db7438e5bda360d423fc3eaa7441c
Marci-0 <vari.marton@gmail.com>
2021-10-08 10:33:22 +0200

    Pótlás

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
-def ask():
-    print('calculator application, please give me the ')
+import secret_logic
 
-    print('1. operand')
+
+def ask():
+    print("calculator application. please give me the ")
+
+    print("1. operand (integer)")
     text = input()
-    while not text.isnumeric():
-        print('bad input, again')
+    while not secret_logic.is_numeric(text):
+        print("bad input, again")
         text = input()
     operand1 = int(text)
 
-    print('operator (+ | - | * | /)')
+    print("operator (+ | - | * | /)")
     text = input()
-    while text not in ['+', '-', '*', '/']:
-        print('bad input, again')
+    while not secret_logic.is_supported_operator(text):
+        print("bad input, again")
         text = input()
-    operator = text
-# teszt
-    print('2. operand')
+    l_operator = text
+
+    print("2. operand (integer)")
     text = input()
-    while not text.isnumeric():
-        print('bad input, again')
+    while not secret_logic.is_numeric(text):
+        print("bad input, again")
         text = input()
     operand2 = int(text)
 
-    return operand1, operator, operand2
+    return operand1, l_operator, operand2
 
 
-def calculate(operand1, p_operator, operand2):
-    rv = 0
-    if p_operator == '+':
-        rv = operand1 + operand2
-    elif p_operator == '-':
-        rv = operand1 - operand2
-    elif p_operator == '*':
-        rv = operand1 * operand2
-    elif p_operator == '/':
-        rv = operand1 / operand2
-    return rv
+# op1, operator, op2 = ask()
+# result = secret_logic.calculate(op1, operator, op2)
 
-
-op1, operator, op2 = ask()
-result = calculate(op1, operator, op2)
-print(f'result: {result}')
+result = secret_logic.calculate(*ask())
+print(f"result: {result}")
 exit(0)
